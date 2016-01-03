262f7052f2a23b3c443e89b06ba8e816917e69a9
Kent Dahl <kent@rubendahl.com>
2016-01-03 23:49:42 +0100

    Basic numerology!

---
+++ nektar-n00b/from-ruby-nuby/tall1.exs
@@ -0,0 +1,14 @@
+# Heltall
+a = 3
+b = 2
+c = a + b    #=> 5
+d = a * b    #=> 6
+
+# Flyttall ved divisjon:
+f = c / b 	 #=> 2.5
+
+# Om du ønsker heltalsdivisjon:
+hele  = div(5, 2)  #=> 2
+halve = rem(5, 2)  #=> 1
+
+IO.puts "tre og to er " <> Integer.to_string(c)
