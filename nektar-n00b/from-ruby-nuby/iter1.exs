aa3a8bc2887bbd0226c13ac7f6435efc4835cbf9
Kent Dahl <kent@rubendahl.com>
2016-01-04 07:53:04 +0100

    Iterating multeplikafjonstabellen.

---
+++ nektar-n00b/from-ruby-nuby/iter1.exs
@@ -0,0 +1,18 @@
+# La oss skrive ut 7-gange-tabellen
+tall = 7
+
+# Elixir har ikke vanlige for-løkker som de fleste andre språk.
+tabell = for i <- (1..10) do
+  "#{i} gange #{tall} er #{i*tall}\n"
+end
+IO.puts tabell
+
+# 5-gange-tabellen
+tall = 5
+
+# Iterasjon ved bruk av Enum(erable) modulen
+(1..10) 
+  |> Enum.map(fn i ->
+      "#{i} gange #{tall} er #{i*tall}\n"
+    end)
+  |> IO.puts
